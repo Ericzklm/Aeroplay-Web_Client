76f4960f6270b71fe31eb5b096168ac400917f43
Ericzklm <52121058+Ericzklm@users.noreply.github.com>
2021-11-09 21:05:24 -0800

    Removed dead code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,40 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Axios from "axios";
 
+function getGlobalStats(data) {
+  let total_score = 0;
+  let total_balloons_popped = 0;
+  let total_accuracy = 0;
+  let total_boosts = 0;
+  let max_score = 0;
+  let max_balloons_popped = 0;
+  let max_accuracy = 0;
+  if (data.length != 0) {
+    for (let item in data) {
+      if (data[item]) {
+        total_score += data[item].score;
+        total_balloons_popped += data[item].balloons_popped;
+        total_accuracy += data[item].shot_accuracy;
+        total_boosts += data[item].boosts_used;
+        if (data[item].score > max_score) {
+          max_score = data[item].score; 
+        }
+        if (data[item].balloons_popped > max_balloons_popped) {
+          max_balloons_popped = data[item].balloons_popped;
+        }
+        if (data[item].shot_accuracy > max_accuracy) {
+          max_accuracy = data[item].shot_accuracy;
+        }
+      }
+    }
+    return [`Maximum Score: ${max_score}`,`Average Score: ${(total_score/data.length).toFixed(2)}`,`Maximum Balloons Popped: ${max_balloons_popped}`,`Total Balloons Popped: ${total_balloons_popped}`,`Average Balloons Popped: ${(total_balloons_popped/data.length).toFixed(2)}`,`Maximum Shot Accuracy: ${max_accuracy.toFixed(2)}`,`Average Shot Accuracy: ${(total_accuracy/data.length).toFixed(2)}`,`Total Boosts: ${total_boosts}`]
+  }
+  else {
+    return ["Stats Unavailable"]
+  }
+}
+
 function App() {
-  const [username, setUsername] = useState("");
   const [gameDataList, setGameDataList] = useState([])
 
   useEffect(() => {
@@ -13,53 +45,6 @@ function App() {
     });
   });
 
-  const submitUsername = () => {
-    Axios.post("https://aeroplay.herokuapp.com/api/user", {
-      username: username
-    }).then(() => {
-      alert("successful insert")
-    })
-  };
-
-  function getGlobalStats(data) {
-    let total_score = 0;
-    let total_balloons_popped = 0;
-    let total_accuracy = 0;
-    let total_boosts = 0;
-    let max_score = 0;
-    let max_balloons_popped = 0;
-    let max_accuracy = 0;
-    if (data.length != 0) {
-      for (let item in data) {
-        if (data[item]) {
-          total_score += data[item].score;
-          total_balloons_popped += data[item].balloons_popped;
-          total_accuracy += data[item].shot_accuracy;
-          total_boosts += data[item].boosts_used;
-          if (data[item].score > max_score) {
-            max_score = data[item].score; 
-          }
-          if (data[item].balloons_popped > max_balloons_popped) {
-            max_balloons_popped = data[item].balloons_popped;
-          }
-          if (data[item].shot_accuracy > max_accuracy) {
-            max_accuracy = data[item].shot_accuracy;
-          }
-        }
-      }
-      return [`Maximum Score: ${max_score}`,`Average Score: ${(total_score/data.length).toFixed(2)}`,`Maximum Balloons Popped: ${max_balloons_popped}`,`Total Balloons Popped: ${total_balloons_popped}`,`Average Balloons Popped: ${(total_balloons_popped/data.length).toFixed(2)}`,`Maximum Shot Accuracy: ${max_accuracy.toFixed(2)}`,`Average Shot Accuracy: ${(total_accuracy/data.length).toFixed(2)}`,`Total Boosts: ${total_boosts}`]
-    }
-    else {
-      return ["Stats Unavailable"]
-    }
-  }
-/*
-<label>Search Name</label>
-<input type="text" name="usernameSearch" onChange = {(e) => {
-  setUsername(e.target.value)
-}}/>
-*/
-<button onClick={ submitUsername }></button>
   return (
     <div className="App">
       <div className="aeroplayHeader">
